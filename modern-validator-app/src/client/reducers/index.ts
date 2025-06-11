ac656ea9bb1e858c77384e31ad286b5ae8363938
Betsalel Williamson <saul.williamson@ieee.org>
2025-06-11 12:00:12 -0700

    nuke and restart from working template https://github.com/jonluca/vite-typescript-ssr-react

--- modern-validator-app/src/client/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import documentsReducer from './documentsReducer';
-
-export default combineReducers({
-    documents: documentsReducer,
-});
--- modern-validator-app/src/store/createStore.ts
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware, Store } from 'redux';
-import { thunk } from 'redux-thunk'; // Use named import
-import rootReducer from '../client/reducers';
-
-export default (): Store => {
-  const store = createStore(rootReducer, {}, applyMiddleware(thunk));
-  return store;
-};
